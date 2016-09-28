f1635291b65706a0a65c2fad6ef0e219d0997371
Blake Hagen <blakehagen@gmail.com>
2016-09-28 17:00:58 -0600

    check for token testing

--- server/features/user/user.server.controller.js
+++ server/features/user/user.server.controller.js
@@ -7,12 +7,9 @@ module.exports = {
   // GET ONE USER BY ID //
   getUser: (req, res) => {
 
-    console.log('req.params ========> ', req.params);
-    console.log('req.headers.authorization', req.headers.authorization);
-
-    // if (!req.headers.authorization) {
-    //   res.status(401).json({message: 'Unauthorized'});
-    // }
+    if (!req.headers.authorization) {
+      res.status(401).send('Unauthorized');
+    }
 
 
     User.findById(req.params.id).select('firstName lastName email').exec((error, user) => {
